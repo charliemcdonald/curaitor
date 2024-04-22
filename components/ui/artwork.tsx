4943a83303de76744643a8b166edcb568db6c05a
Charlie McDonald <c.mcdonald@berkeley.edu>
2024-04-22 12:19:57 -0700

    Increased mobile support; cleaned code

--- components/ui/artwork.tsx
+++ components/ui/artwork.tsx
@@ -1,18 +1,7 @@
 'use client'
 import * as React from 'react';
 import { StaticImageData } from 'next/image';
-import Oil1 from '@/src/Oil1.webp'
-import Oil2 from '@/src/Oil2.webp'
-import Oil4 from '@/src/Oil4.png'
-import aiOil5 from '@/src/aiOil5.webp'
-import RealOil1 from '@/src/RealOil1.jpeg'
-import RealOil2 from '@/src/RealOil2.jpeg'
-import RealOil3 from '@/src/RealOil3.jpeg'
-import RealOil4 from '@/src/RealOil4.jpeg'
-import RealOil5 from '@/src/RealOil5.jpeg'
-import RealOil6 from '@/src/RealOil6.jpeg'
-import RealOil7 from '@/src/RealOil7.jpg'
-import RealOil8 from '@/src/RealOil8.jpg'
+import gallieries from '@/images';
 import frame2 from '@/src/frame2.png'
 
 var aiSelected = -1;
@@ -25,112 +14,7 @@ type Art = {
   title: string
 }
 
-const oilAI1 = {
-  id: 0,
-  image: Oil2,
-  ai: true,
-  title: 'AI Generated',
-}
-
-const oilAI2 = {
-  id: 5,
-  image: Oil4,
-  ai: true,
-  title: 'AI Generated',
-}
-
-const oilAI3 = {
-  id: 8,
-  image: Oil1,
-  ai: true,
-  title: 'AI Generated',
-}
-
-const oilAI4 = {
-  id: 11,
-  image: aiOil5,
-  ai: true,
-  title: 'AI Generated',
-}
-
-const oilReal1 = {
-  id: 1,
-  image: RealOil1,
-  ai: false,
-  title: 'The Beggars by Pieter Bruegel',
-}
-
-const oilReal2 = {
-  id: 2,
-  image: RealOil2,
-  ai: false,
-  title: 'The Arnolfini Portrait by Jan van Eyck',
-}
-
-const oilReal3 = {
-  id: 3,
-  image: RealOil3,
-  ai: false,
-  title: 'Unconscious Patient by Rembrandt',
-}
-
-const oilReal4 = {
-  id: 4,
-  image: RealOil4,
-  ai: false,
-  title: 'St. George & the Dragon by Uccello',
-}
-
-const oilReal5 = {
-  id: 6,
-  image: RealOil5,
-  ai: false,
-  title: 'Saint Jerome in his Study by Antonello da Messina',
-}
-
-const oilReal6 = {
-  id: 7,
-  image: RealOil6,
-  ai: false,
-  title: 'Saint Lucy by Francesco del Cossa',
-}
-
-const oilReal7 = {
-  id: 9,
-  image: RealOil7,
-  ai: false,
-  title: 'The Holy Family by Giulio Romano'
-}
-
-const oilReal8 = {
-  id: 10,
-  image: RealOil8,
-  ai: false,
-  title: 'Lady with an Ermine by Leonardo da Vinci'
-}
-
-const galleries = [
-  [
-    oilAI1,
-    oilReal1,
-    oilReal2,
-  ],
-  [
-    oilReal6,
-    oilAI2,
-    oilReal4,
-  ],
-  [
-    oilReal3,
-    oilReal5,
-    oilAI3,
-  ],
-  [
-    oilAI4,
-    oilReal7,
-    oilReal8,
-  ],
-]
+const galleries = gallieries;
 
 export function revealElem(elem : HTMLElement | null) {
   if (elem == null) {
@@ -171,16 +55,16 @@ export function updateResult(card: Art) {
 export function Artwork(card: Art) {
   console.log(card.title);
   return (
-    <div onClick={() => updateResult(card)} className="group m-4 mt-4 mb-4 relative w-[330px] transition-all hover:w-[495px] duration-500">
-      <img src={frame2.src} className="relative h-[330px] w-[400px] group-hover:h-[495px] group-hover:w-[600px] transition-all duration-500"/>
-      <img src={card.image.src} className='group h-[250px] w-[200px] absolute top-[40px] right-[65px] object-cover group-hover:w-[300px] group-hover:h-[375px] group-hover:top-[60px] group-hover:right-[98px] transition-all duration-500'/>
+    <div onClick={() => updateResult(card)} className="group m-4 mt-4 mb-4 relative w-[330px] transition-all md:hover:w-[495px] duration-500">
+      <img src={frame2.src} className="relative h-[330px] w-[400px] md:group-hover:h-[495px] md:group-hover:w-[600px] transition-all duration-500"/>
+      <img src={card.image.src} className='group h-[250px] w-[200px] absolute top-[40px] right-[65px] object-cover md:group-hover:w-[300px] md:group-hover:h-[375px] md:group-hover:top-[60px] md:group-hover:right-[98px] transition-all duration-500'/>
     </div>
   );
 }
 
 export function Result() {
   return (
-    <div id="testResultSummary" className="bg-black w-2/5 text-white flex flex-col justify-center items-center m-0 rounded-r-[2em]">
+    <div id="testResultSummary" className="bg-black md:w-1/2 w-full text-white flex flex-col justify-center items-center m-0 md:rounded-r-[2em] max-md:rounded-b-[2em]">
       <div id="spacer" className="p-10">
         <img id='resultImg' src={galleries[0][0].image.src} className="w-[300px] h-[300px] object-cover border-2 border-slate-100"/>
       </div>
@@ -234,7 +118,7 @@ export default function Gallery() {
           <button className="text-white text-xl border-2 rounded-full p-3 border-black bg-black" onClick={() => window.location.reload()}>Play Again</button> 
         </div>
       </div>
-      <div id="resultReal" className="transition-opacity duration-1000 opacity-0 absolute flex items-center drop-shadow-lg bg-slate-100 rounded-[2em]">
+      <div id="resultReal" className="transition-opacity duration-1000 opacity-0 absolute flex md:flex-row flex-col items-center drop-shadow-lg bg-slate-100 rounded-[2em]">
         <div className="m-5 w-[550px] h-[350px] flex flex-col items-center justify-center">
           <h1 className="text-center text-4xl pt-10">You chose the <span className="text-green-600 font-bold">real artwork</span></h1>
           <h1 className='text-center py-5 text-xl w-3/4'>This image was painted by a human.<br></br>The AI painting still hangs in the museum.</h1>
---
+++ images.js
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import { StaticImageData } from 'next/image';
+import Oil1 from '@/src/Oil1.webp'
+import Oil2 from '@/src/Oil2.webp'
+import Oil4 from '@/src/Oil4.png'
+import aiOil5 from '@/src/aiOil5.webp'
+import RealOil1 from '@/src/RealOil1.jpeg'
+import RealOil2 from '@/src/RealOil2.jpeg'
+import RealOil3 from '@/src/RealOil3.jpeg'
+import RealOil4 from '@/src/RealOil4.jpeg'
+import RealOil5 from '@/src/RealOil5.jpeg'
+import RealOil6 from '@/src/RealOil6.jpeg'
+import RealOil7 from '@/src/RealOil7.jpg'
+import RealOil8 from '@/src/RealOil8.jpg'
+
+  
+const oilAI1 = {
+  id: 0,
+  image: Oil2,
+  ai: true,
+  title: 'AI Generated',
+}
+
+const oilAI2 = {
+  id: 5,
+  image: Oil4,
+  ai: true,
+  title: 'AI Generated',
+}
+
+const oilAI3 = {
+  id: 8,
+  image: Oil1,
+  ai: true,
+  title: 'AI Generated',
+}
+
+const oilAI4 = {
+  id: 11,
+  image: aiOil5,
+  ai: true,
+  title: 'AI Generated',
+}
+
+const oilReal1 = {
+  id: 1,
+  image: RealOil1,
+  ai: false,
+  title: 'The Beggars by Pieter Bruegel',
+}
+
+const oilReal2 = {
+  id: 2,
+  image: RealOil2,
+  ai: false,
+  title: 'The Arnolfini Portrait by Jan van Eyck',
+}
+
+const oilReal3 = {
+  id: 3,
+  image: RealOil3,
+  ai: false,
+  title: 'Unconscious Patient by Rembrandt',
+}
+
+const oilReal4 = {
+  id: 4,
+  image: RealOil4,
+  ai: false,
+  title: 'St. George & the Dragon by Uccello',
+}
+
+const oilReal5 = {
+  id: 6,
+  image: RealOil5,
+  ai: false,
+  title: 'Saint Jerome in his Study by Antonello da Messina',
+}
+
+const oilReal6 = {
+  id: 7,
+  image: RealOil6,
+  ai: false,
+  title: 'Saint Lucy by Francesco del Cossa',
+}
+
+const oilReal7 = {
+  id: 9,
+  image: RealOil7,
+  ai: false,
+  title: 'The Holy Family by Giulio Romano'
+}
+
+const oilReal8 = {
+  id: 10,
+  image: RealOil8,
+  ai: false,
+  title: 'Lady with an Ermine by Leonardo da Vinci'
+}
+
+export default [
+    [
+      oilAI1,
+      oilReal1,
+      oilReal2,
+    ],
+    [
+      oilReal6,
+      oilAI2,
+      oilReal4,
+    ],
+    [
+      oilReal3,
+      oilReal5,
+      oilAI3
+    ],
+    [
+      oilAI4,
+      oilReal7,
+      oilReal8
+    ],
+]
